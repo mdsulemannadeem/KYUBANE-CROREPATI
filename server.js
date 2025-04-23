9f3a392240ba7a7b6ea3eb2a4a4be363e87fe48e
mdsulemannadeem <mdsulemannadeem.1234@gmail.com>
2025-04-23 14:33:00 +0530

    hi

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ io.on('connection', (socket) => {
 });
 
 // --- Start Server ---
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
